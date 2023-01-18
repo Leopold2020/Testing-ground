c6d4be1ecb740fd6557839873701cdda607d5da6
Gossepojke <svedlund.oskar@gmail.com>
2023-01-18 13:29:24 +0100

    played around

---
+++ Try-except/test1.py
@@ -0,0 +1,26 @@
+
+
+try:
+    a = int(input("test: "))
+
+    
+    #"a" > a
+        
+        
+
+
+
+except SyntaxError:
+    print("Make better code dumbass")
+
+except ValueError:
+    print("Value error")
+
+except TypeError:
+    print("Type error")
+
+except:
+    print("error, unsure")
+
+else:
+    print("it's a miracle, no errors")

3778653524bff4da3daccfd2fceefb83892eaa0d
Oskar S <svedlund.oskar@gmail.com>
2023-09-06 09:42:01 +0200

    made a menu and the abilety to add more wares

--- Grounds/Class/Onlineshop.py
+++ Grounds/Class/Onlineshop.py
@@ -6,32 +6,62 @@
 import OnlineshopResources as res
 
 
+customer = res.Customer(1, "Oskar", "1234")
+ware = res.Ware(1, "Banana", 10, "it's bananas")
 
 def main():
+    user = ""
     order_list = []
     customer_list = []
     ware_list = []
-    
-    
-    customer = res.Customer(1, "Oskar")
-    ware = res.Ware(1, "Banana", 10, "One banana")
     # order = input("What do you want to buy?: ")
     customer_list.append(customer)
     ware_list.append(ware)
     # order_list.append(res.purchase(customer.customer_id, ware.ware_id, ware.price, "Homedelivery"))
     
-    print(customer)
-    print(ware)
-    # for item in order_list:
-    #     print(item)
-
-    make_order(customer_list, ware_list)
+    loginChoice = input("Do you want to login or make an account?\n1. Login\n2. Make account\n>>")
     
+    if loginChoice == "1":
+        login = input("Password\n>>")
+    
+        for account in customer_list:
+            if account.password == login:
+                user = account
+                print(f"Welcome {user.name}")
+    
+    if loginChoice == "2":
+        user = make_account()
+        customer_list.append(user)
+        print(f"Welcome {user.name}")
+    
+
+    while True:
+        menuChoice = input("What do you want to do?\n1. Make order\n2. Show orders\n3. Show wares\n4. Make ware\n5. Exit\n>>")
+        
+        match menuChoice:
+            case "1":
+                order_list.append(make_order(user, ware_list))
+                
+            case "2":
+                for orders in order_list:
+                    print(orders)
+            
+            case "3":
+                for items in ware_list:
+                    print(items)
+                    
+            case "4":  
+                ware_list.append(make_ware())
+                
+            case "5":
+                break
+
     
 def make_account():
     name = input("Name?: ")
     id = input("ID?: ")
-    return res.Customer(id, name)
+    password = input("Password?: ")
+    return res.Customer(id, name, password)
 
 def make_ware():
     name = input("Name?: ")
@@ -40,17 +70,15 @@ def make_ware():
     description = input("Description?: ")
     return res.Ware(id, name, price, description)
 
-def make_order(accounts, wares):
-    identification = int(input("ID?: "))
-    for account in accounts:
-        if account.customer_id == identification:
-            customer = account
-
+def make_order(account, wares):
     purchase = int(input("What do you want to buy?: "))
     for item in wares:
         if item.ware_id == purchase:
             ware = item
+    print(f"{account}, {ware}")
     
-    print(f"{customer}, {ware}")
+    return f"{account.customer_id}, {ware.ware_id}, {ware.price}, Homedelivery"
+    
+    
 if "__main__" == __name__:
     main()
--- Grounds/Class/OnlineshopResources.py
+++ Grounds/Class/OnlineshopResources.py
@@ -4,9 +4,10 @@
 # Online shop made up of classes
 
 class Customer():
-    def __init__(self, customer_id, name) -> None:
+    def __init__(self, customer_id, name, password) -> None:
         self.customer_id = customer_id
         self.name = name
+        self.password = password
         
     def __str__(self) -> str:
         return f"customer_id: {self.customer_id}, name: {self.name}"
